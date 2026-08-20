d8d6cfabcce32dc553a989dfc4b24c7744f84beb
Baldeep Singh <69977181+kambojbaldeep@users.noreply.github.com>
2026-08-20 14:27:27 +0530

    Add solution: 206. Reverse Linked List [python3]

---
+++ Easy/206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,23 @@
+# Reverse Linked List  (#206)  —  Easy
+# https://leetcode.com/problems/reverse-linked-list/
+# Runtime: 0 ms   |   Memory: 20.5 MB
+# Language: Python3
+# Synced: 2026-08-20 via LeetSync
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        prev = None
+
+        while temp is not None:
+            front = temp.next
+            temp.next = prev
+            prev = temp
+            temp = front
+
+        return prev
